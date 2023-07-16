594777334e004184dcf4e95c5c78b41f7ee80c6e
Abby-kr <abby.a.otieno@gmail.com>
2023-07-16 19:36:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+let pickupLocation;
+let result;
+function distanceFromHqInBlocks(num){
+    result = Math.abs(num - 42)
+    return result;
+}
+function distanceFromHqInFeet(num){
+    result = distanceFromHqInBlocks(num);
+    return result * 264
+}
+let travelledInFeet;
+function distanceTravelledInFeet(a,b){
+    travelledInFeet =  Math.abs(((b-a)*264))
+    return travelledInFeet;
+}
+let fare;
+function calculatesFarePrice(a,b){  
+    distanceTravelledInFeet(a,b);
+         if(travelledInFeet <=400){
+             fare = 0
+             return fare;
+        }
+        else if(travelledInFeet > 400 && travelledInFeet < 2000){
+             fare = (travelledInFeet-400) * 0.02
+             return fare;
+        }
+        else if(travelledInFeet > 2000 && travelledInFeet < 2500){
+            fare = 25;
+            return fare;
+        }
+        else {
+            fare = 'cannot travel that far'
+            return fare;
+        }
+}
